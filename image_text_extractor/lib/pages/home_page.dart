e302fc4001b7c00837687c7ee40f8511c7bb8aaa
Michał Zychowicz <zychomichal@gmail.com>
2024-12-15 10:14:01 +0100

    Add deletion confirmation modal

--- image_text_extractor/lib/pages/home_page.dart
+++ image_text_extractor/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_text_extractor/models/note_collection.dart';
+import 'package:image_text_extractor/models/note_item.dart';
 import 'package:image_text_extractor/pages/camera_view_page.dart';
 import 'package:image_text_extractor/pages/extract_text_page.dart';
 import '../widgets/header_bar.dart';
@@ -57,16 +58,15 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     );
   }
 
-
   Widget createBody() {
     return ListView(
       children: NoteCollection.notes.map((note) {
         return Container(
-            margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-        decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(5),
-        ),
+          margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(5),
+          ),
           child: ListTile(
             title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -75,27 +75,54 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                 IconButton(
                   icon: const Icon(Icons.delete),
                   onPressed: () {
-                    NoteCollection.remove(note);
-                    setState(() {});
+                    _showConfirmationDialog(context, note);
                   },
                 ),
               ],
             ),
-          subtitle: Text(note.content),
-          onTap: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ExtractTextPage(
-                  imagePath: note.imagePath,
-                  noteItem: note,
+            subtitle: Text(note.content),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ExtractTextPage(
+                    imagePath: note.imagePath,
+                    noteItem: note,
+                  ),
                 ),
-              ),
-            )
-          },
-        )
+              )
+            },
+          ),
         );
       }).toList(),
+    );
+  }
+
+  void _showConfirmationDialog(BuildContext context, NoteItem note) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Deletion'),
+          content: Text('Are you sure you want to delete the note "${note.title}"'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                NoteCollection.remove(note);
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
